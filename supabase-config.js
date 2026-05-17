31905438c1cb188de4f27e2ee103d03039a1bb8a
eastoceangreat-arch <eastoceangreat@gmail.com>
2026-05-17 13:43:00 +0800

    Update supabase-config.js
    Added supabase key

--- supabase-config.js
+++ supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = window.SUPABASE_CONFIG || {
-  url: "",
-  publishableKey: "",
+  url: "https://lepqplkdtnbckizhrfke.supabase.co/rest/v1/",
+  publishableKey: "sb_publishable_27BpmT-CG_wrpR4nQDoxuQ_xOT1wVpi",
 };
